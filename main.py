f3fd2d1e53091003a41a4d4eb518ca2b0d87d44a
patelkartavya2015-gif <patelkartavya2015@gmail.com>
2025-10-14 11:07:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def circumfrence(radius):
+    pi = 3.14159
+    return 2 * pi * radius
+
+r = float(input("Enter the radius of the circle(Even if it is a whole numbr you will have to enter like 5.0 and 6.0): "))
+
+print("The circumfrence of the circle is:", circumfrence(r))
