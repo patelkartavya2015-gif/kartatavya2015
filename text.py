24a54a94d4c38c79eeba1392162e9df9258f9de8
patelkartavya2015-gif <patelkartavya2015@gmail.com>
2026-02-02 18:21:53 +0530

    Add files via upload

--- text.py
+++ text.py
@@ -1,19 +1,36 @@
-import sys as s
-import pygame as p
-p.init()
-screen = p.display.set_mode((800, 500))
-screen.fill((58, 58, 58))
-p.display.set_caption("My Pygame Window")
+import tkinter as tk
+from datetime import date
 
-font = p.font.SysFont("Arial", 90)
-text_surface = font.render("Hello, Pygame!", True, (255, 255, 255))
-rect = p.Rect(50, 200, 400, text_surface.get_height())
+root = tk.Tk()
+root.geometry("300x200")
+root.title("Date Display")
+root.resizable(True, True)
 
-running = True
-while running:
-    for event in p.event.get():
-        if event.type == p.QUIT:
-            running = False
-    
-    screen.blit(text_surface, rect)
-    p.display.flip()
+lab1 = tk.Label(root, text="Hey There!", font=("Arial", 16), bg="#0800FF")
+
+namelbl = tk.Label(root, text="Please Enter Your Name:", font=("Arial", 12))
+
+name_entry = tk.Entry(root, font=("Arial", 12))
+
+textbox = tk.Text(root, font=("Arial", 12), height=5, width=30)
+
+def display():
+    name = name_entry.get()
+    global message
+    message = "Welcome to the application.\nToday's date is:"
+
+    greet = "Hello " + name + "!\n"
+
+    textbox.insert(tk.END, greet)
+    textbox.insert(tk.END, "\n" + message)
+    textbox.insert(tk.END, "\n" + str(date.today()))
+
+btn = tk.Button(root, text="Submit", font=("Arial", 12), bg="DarkBlue", fg ="White", command=lambda: display())
+
+lab1.pack(pady=10)
+namelbl.pack()
+name_entry.pack(pady=5)
+btn.pack(pady=10)
+textbox.pack(pady=10)
+
+root.mainloop()
